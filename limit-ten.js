bf110542b3ed925f2431c083bbaca368cc5b7b6a
Brian Doane <brianmdoane@gmail.com>
2016-08-26 14:05:44 -0500

    event stream map methos returning words matching argument

--- limit-ten.js
+++ limit-ten.js
@@ -1,14 +1,17 @@
 #!/usr/bin/env node
 "use strict"
 
+const [,, ...arg] = process.argv
 const { Transform } = require('stream')
+const { map } = require('event-stream')
 
-
-const ts = Transform({
-	// Enhanced object literal
-	transform (buff, enc, cb) {
-		cb(null, buff.toString().toUpperCase())
+const findWord = map((line, cb) => {
+	// CLI args come in as strings and not necessary to interp
+	if (line.toString().startsWith(arg[0])) {
+		cb(null, line.toString())
 	}
+	// This says don't pass anything down pipe
+	cb()
 })
 
 // // Example of catching an error
@@ -16,4 +19,4 @@ const ts = Transform({
 // 	console.log('I found an error')
 // })
 
-module.exports = ts
+module.exports = findWord
--- word-search.js
+++ word-search.js
@@ -2,45 +2,14 @@
 // @morecallan
 "use strict"
 
-const [,, ...arg] = process.argv
+
 const { createReadStream } = require('fs')
-const es = require('event-stream')
 const transformer = require('./limit-ten')
-const searchTerm = `${arg}`
+const { split } = require('event-stream')
 
-const readStream = createReadStream('/usr/share/dict/words', { highWaterMark: 1 })
-
-readStream.on('data', buffer => {
-	readStream.pause(buffer.toString())
-	//process.stdout.write(buffer.toString())
-})
-
-const timer = setInterval(() => readStream.resume(), 50)
-
-readStream.on('end', () => {
-	console.log('end')
-	clearInterval(timer)
-})
+const readStream = createReadStream('/usr/share/dict/words')
 
 readStream
+	.pipe(split())
 	.pipe(transformer)
 	.pipe(process.stdout)
-
-
-// const readStream = createReadStream('/usr/share/dict/words', { highWaterMark: 1 })
-
-// readStream.on('data', buffer => {
-// 	readStream.pause(buffer.toString())
-// 	//process.stdout.write(buffer.toString())
-// })
-
-// const timer = setInterval(() => readStream.resume(), 50)
-
-// readStream.on('end', () => {
-// 	console.log('end')
-// 	clearInterval(timer)
-// })
-
-// readStream
-// 	.pipe(transformer)
-// 	.pipe(process.stdout)
